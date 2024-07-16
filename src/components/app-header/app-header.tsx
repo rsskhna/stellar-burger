600bb7ec85fa2026dca29b6e3f3a8e2e19d972bc
Irina Rossokhina <i.rossokhina@yandex.ru>
2024-07-16 14:25:51 +0300

    fixed bugs

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import { AppHeaderUI } from '@ui';
-import { useSelector } from 'react-redux';
-import { selectUserData, selectUserName } from '../../services/userSlice';
+import { useSelector } from '../../services/store';
+import { selectUserName } from '../../services/userSlice';
 
 export const AppHeader: FC = () => {
   const userName = useSelector(selectUserName);
